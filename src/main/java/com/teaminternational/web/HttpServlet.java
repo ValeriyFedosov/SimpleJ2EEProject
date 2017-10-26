461794ef84cf466ffadb501e3cc7cb8ea5a48c2d
Valeriy <fedosv.valera95@gmail.com>
2017-10-26 16:40:02 +0300

    HttpServlet added

--- src/main/java/com/teaminternational/web/HttpServlet.java
+++ src/main/java/com/teaminternational/web/HttpServlet.java
@@ -1,6 +1,38 @@
 package com.teaminternational.web;
 
 
+import com.teaminternational.HelloWorldGenerator;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import static java.awt.Color.white;
+
 public class HttpServlet extends BaseHttpServlet{
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        HelloWorldGenerator asd = getBean(HelloWorldGenerator.COMPONENT_NAME);
+        asd.sayHello();
+
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>Hello</title>");
+        out.println("</head>");
+        out.println("<body bgcolor=\"white\">");
+        out.println(asd.sayHello());
+        out.println("</body>");
+        out.println("</html>");
+
+
+
+
+          }
 
 }
